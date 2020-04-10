9552a32c2b6125484b31d8151ffbf8c93a7c359c
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-04-10 11:11:34 -0400

    Made axios request to the api and was able to get a response of all the players

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from "axios";
 import './App.css';
 
 class App extends React.Component {
   constructor(){
     super();
+  }
+
+  componentDidMount(){
+    axios.get('http://localhost:5000/api/players')
+    .then(response => {
+      console.log(response.data);
+    })
   }
 
   render(){
